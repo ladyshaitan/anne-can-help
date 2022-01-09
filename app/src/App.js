a5519cf42d2c74ddc987c2540b919cf1a4b998ba
Anne Pruett <anne.pruett@autodesk.com>
2022-01-09 12:58:01 -0800

    Add graph background, typewriter header

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typewriter from "typewriter-effect";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  if (process.env.REACT_APP_UNDER_CONSTRUCTION === "true") {
+    console.log({
+      REACT_APP_UNDER_CONSTRUCTION: process.env.REACT_APP_UNDER_CONSTRUCTION,
+      result: !process.env.REACT_APP_UNDER_CONSTRUCTION,
+    });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Anne's website is under construction ⚒️</p>
+        </header>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Anne's website is under construction ⚒️
-        </p>
-      </header>
+    <div>
+      <div alt="" className="vanta-bg" id="vanta-bg" />
+      <div className="Typewriter-container">
+        <Typewriter
+          options={{
+            wrapperClassName: "Typewriter",
+            strings: ["Hello world...", "This is Anne", "Is anyone there...?"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
